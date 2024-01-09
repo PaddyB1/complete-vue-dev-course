14720e9c958d07d0137aa3822992463ef09843d1
Paddy Borthwick <paddy.borthwick@mtf.co.nz>
2024-01-10 11:33:52 +1300

    Starting 27

--- ZTM Vue Course Code/Section 2/section-2-1-STARTER/app.js
+++ ZTM Vue Course Code/Section 2/section-2-1-STARTER/app.js
@@ -2,16 +2,43 @@ const vm = Vue.createApp({
     data() {
         return {
             firstName: 'John',
+            middleName: '',
             lastName: 'Doe',
-			url: "https://google.com"
+			url: 'https://google.com',
+            raw_url : '<a href="https://google.com" target="_blank">Google</a>',
+            age: 20
         }	
     },
 	
 	methods: {
-		fullName() {
-			return `${this.firstName} ${this.lastName.toUpperCase()}`
+        increment(){
+            this.age++
+        },
+        updateLastName(msg, event){
+            this.lastName = event.target.value
+            
+            //console.log(msg)
+        },
+        updateMiddleName(event){
+            this.middleName = event.target.value
+        }
+	},
+
+    computed:{
+        fullName() {
+            console.log('Full name computed property was called')
+
+			return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
 		}
-	}
+    },
+
+    watch:{
+        age(newVal, oldVal){
+            setTimeout(() =>{
+                this.age =20
+            },3000)
+        }
+    }
 }).mount('#app')
 
 // setTimeout(() => {
