3cacaa2f588aae5873c8658d1b46d53108468892
Paddy Borthwick <paddy.borthwick@mtf.co.nz>
2024-01-10 13:22:34 +1300

    finished 28

--- ZTM Vue Course Code/Section 2/section-2-18-STARTER/app.js
+++ ZTM Vue Course Code/Section 2/section-2-18-STARTER/app.js
@@ -1,2 +1,15 @@
 let vm = Vue.createApp({
+    data(){
+        return {
+            isPurple: false,
+            selectedColour: '',
+            size: 150
+        }
+    },
+
+    computed:{
+        circle_classes(){
+            return { purple: this.isPurple }
+        }
+    }
 }).mount('#app')
