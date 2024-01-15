ce0738666c6af1c98dd9ee75f349c019a27f8b72
Paddy Borthwick <paddy.borthwick@mtf.co.nz>
2024-01-15 16:53:36 +1300

    fixed a bunch of weird tailwind errors

---
+++ ZTM Vue Course Code/music/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.{js,ts,jsx,tsx,vue}'] },
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
