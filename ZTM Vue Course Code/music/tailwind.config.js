0e9453a5249bc48196df6fa9551dba0c55250b3c
Paddy Borthwick <paddy.borthwick@mtf.co.nz>
2024-01-16 10:20:25 +1300

    section 9 done

--- ZTM Vue Course Code/music/tailwind.config.js
+++ ZTM Vue Course Code/music/tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.{js,ts,jsx,tsx,vue}'] },
+  purge: { content: ['*.html', './src/**/*.{js,ts,jsx,tsx,vue}'] },
   darkMode: false,
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      sans: [
+        'ui-sans-serif',
+        'system-ui',
+        'sans-serif',
+        'Roboto',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"'
+      ]
+    }
   },
   variants: {
     extend: {}
